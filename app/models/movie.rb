ddef6abaa0de799c58bc28dd1696372dfc637067
Tate <thandley1234@gmail.com>
2023-01-13 11:51:39 -0700

    commit tree 6ffd3a50f25310b67c977fe3b8865c258989093f

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        movie.create(title: title)
+    end
+    def self.first_movie
+        movie.first
+    end
+
 end
+
